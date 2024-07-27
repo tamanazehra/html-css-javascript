d4c73947c326fd34f7d1b927db31c2fb2179b9c7
tamanazehra <zehratamana3@gmail.com>
2024-07-27 11:38:06 +0530

    practice

--- JavaScript/first.js
+++ JavaScript/first.js
@@ -1,13 +1,13 @@
-//  Arithmetic Operators in JavaScript.
+//  Unary Operators in JavaScript.
+
 
 let a = 5;
 let b = 3;
 
 console.log("a =" , a, "" ,  "b =" , b);
-console.log("a + b =" , a + b);
-console.log("a - b =" , a - b);
-console.log("a * b =" , a * b);
-console.log("a / b =" , a / b);
-console.log("a % b =" , a % b);
-console.log("a ** b =" , a ** b); //** is used for a^b
 
+a++ // a = a + 1
+console.log( "a =" , a)
+
+a-- // a = a - 1
+console.log( "a =" , a)

1a63607d15a86e75b63fdc6b925a0938dc643c86
tamanazehra <zehratamana3@gmail.com>
2024-08-06 13:43:05 +0530

    practice

---
+++ JavaScript/script.js
@@ -0,0 +1,33 @@
+            // learning JS
+
+console.log("hello world");
+console.log("Horizon IT");
+console.log("My First Code in JS")
+
+
+firstName = "Tamana";
+lastName = "Zehra";
+address = "New York ,2nd Street";
+isAdult = true;
+
+console.log(firstName);
+console.log(lastName);
+console.log(address);
+console.log(isAdult);
+
+// types of variables
+
+var a = 1;
+ console.log(a);  // var: can re deaclear , update  
+var a = 10;    
+console.log(a);
+
+let b = 2;
+console.log(b);    //let: cannot redeclear  , can update
+b = 3;
+console.log(b);
+
+const c = 100;
+console.log(c);  //const: neither can redeclear not update 
+
+

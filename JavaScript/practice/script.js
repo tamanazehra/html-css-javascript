357e68a4f9e862aa21098b2e2b98310f1120965c
tamanazehra <zehratamana3@gmail.com>
2024-08-06 20:34:34 +0530

    practice

--- JavaScript/practice/script.js
+++ JavaScript/practice/script.js
@@ -1,9 +1,25 @@
-            //Praticing Conditional Statements
+            //Practicing Conditional Statements
 
-let num = prompt("Enter number:");
-    if(num % 5 == 0){
-        console.log(num, "is  multiple of 5");
-    }
-    else{
-        console.log(num, "is not multiple of 5");
-    }
+/* Writing a code which can give grades to students according to their scores
+                ~90 - 100 : A
+                ~80 - 89 : B
+                ~70 - 79 : C
+                ~60 - 69 : D
+                ~50 - 59 : E
+                ~0 - 49 : FAIL!! */
+
+    let score = prompt("ENTER YOUR SCORE:");
+    let grade;
+            if(score >= 90 && score<=100){
+                console.log("YOUR GRADE IS", grade = "A");
+            } else if(score >= 80 && score <= 89){
+                console.log("YOUR GRADE IS", grade = "B");
+            }else if(score >= 70 && score <= 79){
+                console.log("YOUR GRADE IS", grade = "C");
+            }else if(score >= 60 && score <= 69){
+                console.log("YOUR GRADE IS", grade = "D");    
+            }else if(score >= 50 && score <= 59){
+                console.log("YOUR GRADE IS", grade = "E");    
+            }else if(score >= 0 && score <= 49){
+                console.log("YOUR GRADE IS", grade = "FAIL!");    
+            }

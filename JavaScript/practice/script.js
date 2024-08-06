459d9ae60a9a1a9e44ef931e6dba7caf66bc6e5c
tamanazehra <zehratamana3@gmail.com>
2024-08-06 20:07:24 +0530

    practice

---
+++ JavaScript/practice/script.js
@@ -0,0 +1,9 @@
+            //Praticing Conditional Statements
+
+let num = prompt("Enter number:");
+    if(num % 5 == 0){
+        console.log(num, "is  multiple of 5");
+    }
+    else{
+        console.log(num, "is not multiple of 5");
+    }

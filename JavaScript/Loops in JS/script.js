cddce4c071a3efc475f22e93447be80ec11c21ab
tamanazehra <zehratamana3@gmail.com>
2024-08-07 13:03:59 +0530

    practice

--- JavaScript/Loops in JS/script.js
+++ JavaScript/Loops in JS/script.js
@@ -1,14 +1,32 @@
-                                   //for loop
+//                                    //for loop
 
-//     for (let i = 1; i <= 1000; i++){              // keyword (initialization; condition; updation)
+//     for (let i = 1; i <= 100; i++){              // keyword (initialization; condition; updation)
 //            console.log("HELLO WORLD!")                      
 //     }  
 //     console.log("loop has ended")                                            
    
-                     //Calculating sum of 1 to 100 using for loop
+//                      //Calculating sum of 1 to 100 using for loop
 
-       sum = 0;
-              for(i = 1; i <= 100; i++){
-                     sum = sum + i 
-              }
-       console.log("sum =", sum);
+//        sum = 0;
+//               for(i = 1; i <= 100; i++){
+//                      sum = sum + i 
+//               }
+//        console.log("sum =", sum);
+
+//                                    // While loop 
+              
+//        let i = 1;                                               //intialization
+//               while (i<=100){                                  // keyword (condition){
+//                      console.log("HELLO WORLD!")                      //statement
+//                      i++;                                               //updation
+//               }                                               
+
+
+//                                    // Do-While loop
+        
+                                   
+//         let i = 20;                                                          //initialization
+//               do{                                                             //keyword
+//                      console.log("HELLO WORLD!");                            //statement
+//                      i++;                                                    //updation
+//               } while (i <=10);                                              //condition

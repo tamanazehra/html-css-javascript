e1eb294614142b586a19de5cc99538f927feb3b0
tamanazehra <zehratamana3@gmail.com>
2024-08-07 11:51:46 +0530

    practice

---
+++ JavaScript/Loops in JS/script.js
@@ -0,0 +1,6 @@
+            //for loop
+
+    for (let i = 1; i <= 10; i++){              // keyword (initialization; condition; updation)
+           console.log("HELLO WORLD!")                      
+    }                                              
+   

90071b2dc573f945b3e322c6245940eeebd6c1fb
tamanazehra <zehratamana3@gmail.com>
2024-08-07 12:03:39 +0530

    practice

--- JavaScript/Loops in JS/script.js
+++ JavaScript/Loops in JS/script.js
@@ -1,6 +1,14 @@
-            //for loop
+                                   //for loop
 
-    for (let i = 1; i <= 10; i++){              // keyword (initialization; condition; updation)
-           console.log("HELLO WORLD!")                      
-    }                                              
+//     for (let i = 1; i <= 1000; i++){              // keyword (initialization; condition; updation)
+//            console.log("HELLO WORLD!")                      
+//     }  
+//     console.log("loop has ended")                                            
    
+                     //Calculating sum of 1 to 100 using for loop
+
+       sum = 0;
+              for(i = 1; i <= 100; i++){
+                     sum = sum + i 
+              }
+       console.log("sum =", sum);

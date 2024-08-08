9ad0889822b73b17cade47a021003546e10d7671
tamanazehra <zehratamana3@gmail.com>
2024-08-08 10:31:19 +0530

    practice

---
+++ JavaScript/Arrays in JS/script.js
@@ -0,0 +1,41 @@
+            //                     //Arrays in JS
+            //             // Collection of items 
+
+            // let marks = [97, 85, 39, 76, 90, 76];
+            // console.log(marks);
+
+            // let heroes = ["Ironman", "Batman ", "Captain America", "Hulk", "Thor", "Spiderman", "Doctor Strange", "Black Widow", "Wolverine", "Black Panther", "Scarlet Witch"];
+            // console.log(heroes);
+            
+            //                     // Array Indices
+            //         console.log(heroes[3]);
+            //         console.log(heroes[8]);
+            //         console.log(heroes[10]);
+
+            //                         //Looping over an Array
+            //                     //for loop
+            //     for(let idx=0; idx<heroes.length; idx++){
+            //             console.log(heroes[idx])
+            //     };
+
+            //                     //for-of loop
+            //     for (let hero of heroes){
+            //         console.log(hero);
+            //     };
+
+                // Finding the avg marks of the entire class 
+                //marks of students ->[85, 97, 44, 37, 76, 60]
+
+            //     let marks = [85, 97, 44, 37, 76, 60];
+            //     console.log(marks);
+
+            //     let sum = 0;
+
+            //     for(let val of marks){
+            //         sum += val;             
+            //     }
+            //    let avg = sum / marks.length;
+            //    console.log(`avg marks of the class is ${avg}`);
+
+
+                

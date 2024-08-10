c8f0702b84b5cdadb9ec77cde234c82af1f9e020
tamanazehra <zehratamana3@gmail.com>
2024-08-10 11:55:16 +0530

    practice

--- JavaScript/Functions in JS/script.js
+++ JavaScript/Functions in JS/script.js
@@ -11,4 +11,11 @@
             myFunction();                   //helps to avoid redundency in our code
 
 
-            
+                            // creating a function for sum of 2 numbers
+
+                function sum(x, y) {
+                    console.log(x + y);
+                }
+
+                sum(10, 20);
+                sum(14, 24);

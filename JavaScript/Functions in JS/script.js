8b8c06c7a46fb602f0414be37209631d3e3afc0a
tamanazehra <zehratamana3@gmail.com>
2024-08-10 11:49:56 +0530

    practice

--- JavaScript/Functions in JS/script.js
+++ JavaScript/Functions in JS/script.js
@@ -1,3 +1,14 @@
                                 //Functions in JS
 
-                                
+            function myFunction() {
+                console.log("learning JS");
+                console.log("hello there!!");
+                console.log("learning JS");
+                console.log("hello there!!");
+            } 
+            
+            myFunction();
+            myFunction();                   //helps to avoid redundency in our code
+
+
+            

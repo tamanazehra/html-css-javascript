d4a933254d037b842f3c3f2ddfc8ea5a9c713ad1
tamanazehra <zehratamana3@gmail.com>
2024-08-10 13:11:36 +0530

    practice

--- JavaScript/Functions in JS/script.js
+++ JavaScript/Functions in JS/script.js
@@ -45,6 +45,28 @@
 
                 arrowProd(12, 8);
 
+                            //creating a function using the "function" keyword that takes a string as an argument
+                            //& return the number of vowels in the string
+
+                            function countVowels(str) {
+                                let count = 0;
+                                                              
+                                for (let char of str) {
+                                    if (char === "a" ||
+                                        char === "e" ||
+                                        char === "i" || 
+                                        char === "o" || 
+                                        char === "u"
+                                    ) {
+                                        count++;
+                                    }
+                                }
+
+                                
+                                return count;
+                            }
+                               
+
 
 
 

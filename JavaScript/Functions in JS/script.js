650fa070267c4e193057c812e2de7f251886563e
tamanazehra <zehratamana3@gmail.com>
2024-08-10 12:23:57 +0530

    practice

--- JavaScript/Functions in JS/script.js
+++ JavaScript/Functions in JS/script.js
@@ -19,3 +19,32 @@
 
                 sum(10, 20);
                 sum(14, 24);
+
+                            // creating a function for multiplication of 2 numbers
+                
+                function prod(x, y){
+                    console.log(x * y);
+                }
+
+                prod(2, 4);
+                prod(3, 7);
+
+                                //Arrow function  (compact way of writing functions)
+
+                        //sum function
+                const arrowSum = (x, y) => {
+                    console.log(x + y);
+                }
+
+                 arrowSum(20, 40);
+
+                        //mul function
+                const arrowProd = (x, y) => {
+                    console.log(x * y);
+                }
+
+                arrowProd(12, 8);
+
+
+
+

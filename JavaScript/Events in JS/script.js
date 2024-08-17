d8c8f169e0754f8df4d0c11237c7165cb515e6c5
tamanazehra <zehratamana3@gmail.com>
2024-08-17 15:40:57 +0530

    practice

---
+++ JavaScript/Events in JS/script.js
@@ -0,0 +1,20 @@
+ //creating a toggle button that changes the screen to dark-mode when clicked and light-mode when clicked again
+
+ let modeBtn = document.querySelector("#mode");
+ let body = document.querySelector("body");
+ let currMode = "light";
+
+
+ modeBtn.addEventListener("click", () => {
+    if(currMode === "light"){
+        currMode = "dark";
+        body.classList.add("dark");
+        body.classList.remove("light");
+    } else {
+        currMode = "light";
+        body.classList.add("light");
+        body.classList.remove("dark");
+    };
+
+    console.log(currMode);
+ })

f74701aec399e36cc402d1607b5ee7decbba10ed
tamanazehra <zehratamana3@gmail.com>
2024-09-23 10:41:09 +0530

    assignment

---
+++ assignments/assignment-2a/script.js
@@ -0,0 +1,12 @@
+function showDays() {
+    var daysList = document.getElementById('daysList');
+    daysList.innerHTML = `
+        <li>Sunday</li>
+        <li>Monday</li>
+        <li>Tuesday</li>
+        <li>Wednesday</li>
+        <li>Thursday</li>
+        <li>Friday</li>
+        <li>Saturday</li>
+    `;
+}

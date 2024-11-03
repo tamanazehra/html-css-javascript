18eb10a0e6e7a71f6be80a28f14223793f00500c
tamanazehra <zehratamana3@gmail.com>
2024-11-03 21:12:51 +0530

    practice

--- mini-projects/To-Do List/script.js
+++
@@ -1,9 +0,0 @@
-function addTask(){
-    var task = document.getElementById('task').value;
-    var li = document.createElement('li');
-       
-        li.textContent = task;
-        document.getElementById('list').appendChild(li);
-        document.getElementById('task').value = '';
-      
-};
